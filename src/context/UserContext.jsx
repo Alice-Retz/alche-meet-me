a7a4557aeebf96bd7a8b48e20490e4bdc6c0f34f
Alice-Retz <alicegretz@gmail.com>
2021-12-13 19:18:13 -0600

    Working as intended

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,45 +1,29 @@
-import { createContext, useContext, useState } from 'react'
+import { useContext, useState, createContext, useEffect } from 'react'
+import { fetchUser } from '../services/user'
 
 const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({})
-  const [name, setName] = useState('')
-  const [avatar, setAvatar] = useState('')
-  const [likes, setLikes] = useState([])
-  const [motto, setMotto] = useState('')
-  const [color, setColor] = useState('')
-  const [header, setHeader] = useState('')
 
-  return (
-    <UserContext.Provider
-      value={{
-        user,
-        setUser,
-        name,
-        setName,
-        avatar,
-        setAvatar,
-        likes,
-        setLikes,
-        motto,
-        setMotto,
-        color,
-        setColor,
-        header,
-        setHeader,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  )
+  useEffect(() => {
+    fetchUser()
+      .then((fetchedUser) => {
+        setUser(fetchedUser)
+      })
+      .catch((error) => {
+        throw new Error(`Error: ${error}`)
+      })
+  }, [])
+
+  return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
 }
 
 const useUser = () => {
   const context = useContext(UserContext)
 
   if (context === undefined) {
-    throw new Error('Whoops-a-daisy, something went wrong')
+    throw new Error('useUser hook must be called within a UserProvider')
   }
 
   return context
